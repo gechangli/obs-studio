9e730d4f99efabb5b319187a3544fd78434fe23c
luma <stubma@gmail.com>
2017-08-24 11:14:57 +0800

    remove profile, scene config

--- obs-lite/Classes/ui/obs_app.cpp
+++ obs-lite/Classes/ui/obs_app.cpp
@@ -21,9 +21,6 @@ using namespace std;
 #define DEFAULT_LANG "en_US"
 
 bool portable_mode = false;
-string opt_starting_collection;
-string opt_starting_profile;
-string opt_starting_scene;
 
 OBSApp::OBSApp(profiler_name_store_t *store) :
 profilerNameStore(store) {
@@ -32,44 +29,6 @@ profilerNameStore(store) {
 
 OBSApp::~OBSApp() {
     
-}
-
-void OBSApp::AddExtraModulePaths() {
-    char base_module_dir[512];
-#if defined(_WIN32) || defined(__APPLE__)
-    int ret = GetProgramDataPath(base_module_dir, sizeof(base_module_dir),
-                                 "obs-studio/plugins/%module%");
-#else
-    int ret = GetConfigPath(base_module_dir, sizeof(base_module_dir),
-                            "obs-studio/plugins/%module%");
-#endif
-    
-    if (ret <= 0)
-        return;
-    
-    string path = (char*)base_module_dir;
-#if defined(__APPLE__)
-    obs_add_module_path((path + "/bin").c_str(), (path + "/data").c_str());
-    
-    BPtr<char> config_bin = os_get_config_path_ptr("obs-studio/plugins/%module%/bin");
-    BPtr<char> config_data = os_get_config_path_ptr("obs-studio/plugins/%module%/data");
-    obs_add_module_path(config_bin, config_data);
-    
-#elif ARCH_BITS == 64
-    obs_add_module_path((path + "/bin/64bit").c_str(),
-                        (path + "/data").c_str());
-#else
-    obs_add_module_path((path + "/bin/32bit").c_str(),
-                        (path + "/data").c_str());
-#endif
-}
-
-int OBSApp::GetProgramDataPath(char *path, size_t size, const char *name) {
-    return os_get_program_data_path(path, size, name);
-}
-
-char* OBSApp::GetProgramDataPathPtr(const char *name) {
-    return os_get_program_data_path_ptr(name);
 }
 
 int OBSApp::GetConfigPath(char *path, size_t size, const char *name) {
@@ -97,12 +56,6 @@ bool OBSApp::StartupOBS(const char* locale) {
     if (!InitGlobalConfig())
         throw "Failed to initialize global config";
     
-    // set default values
-    config_set_default_string(globalConfig, "Basic", "Profile", "Untitled");
-    config_set_default_string(globalConfig, "Basic", "ProfileDir", "Untitled");
-    config_set_default_string(globalConfig, "Basic", "SceneCollection", "Untitled");
-    config_set_default_string(globalConfig, "Basic", "SceneCollectionFile", "Untitled");
-    
     // load config
     if (GetConfigPath(path, sizeof(path), "data/plugin_config") <= 0) {
         return false;
@@ -122,115 +75,11 @@ bool OBSApp::StartupOBS(const char* locale) {
     return true;
 }
 
-string OBSApp::GetProfileDirFromName(const char *name) {
-    string outputPath;
-    os_glob_t *glob;
-    char path[512];
-    
-    if (GetConfigPath(path, sizeof(path), "data/profiles") <= 0)
-        return outputPath;
-    
-    strcat(path, "/*");
-    
-    if (os_glob(path, 0, &glob) != 0)
-        return outputPath;
-    
-    for (size_t i = 0; i < glob->gl_pathc; i++) {
-        struct os_globent ent = glob->gl_pathv[i];
-        if (!ent.directory)
-            continue;
-        
-        strcpy(path, ent.path);
-        strcat(path, "/basic.ini");
-        
-        ConfigFile config;
-        if (config.Open(path, CONFIG_OPEN_EXISTING) != 0)
-            continue;
-        
-        const char *curName = config_get_string(config, "General", "Name");
-        if (astrcmpi(curName, name) == 0) {
-            outputPath = ent.path;
-            break;
-        }
-    }
-    
-    os_globfree(glob);
-    
-    if (!outputPath.empty()) {
-        replace(outputPath.begin(), outputPath.end(), '\\', '/');
-        const char *start = strrchr(outputPath.c_str(), '/');
-        if (start)
-            outputPath.erase(0, start - outputPath.c_str() + 1);
-    }
-    
-    return outputPath;
-}
-
-string OBSApp::GetSceneCollectionFileFromName(const char *name) {
-    string outputPath;
-    os_glob_t *glob;
-    char path[512];
-    
-    if (GetConfigPath(path, sizeof(path), "data/scenes") <= 0)
-        return outputPath;
-    
-    strcat(path, "/*.json");
-    
-    if (os_glob(path, 0, &glob) != 0)
-        return outputPath;
-    
-    for (size_t i = 0; i < glob->gl_pathc; i++) {
-        struct os_globent ent = glob->gl_pathv[i];
-        if (ent.directory)
-            continue;
-        
-        obs_data_t *data =
-        obs_data_create_from_json_file_safe(ent.path, "bak");
-        const char *curName = obs_data_get_string(data, "name");
-        
-        if (astrcmpi(name, curName) == 0) {
-            outputPath = ent.path;
-            obs_data_release(data);
-            break;
-        }
-        
-        obs_data_release(data);
-    }
-    
-    os_globfree(glob);
-    
-    if (!outputPath.empty()) {
-        outputPath.resize(outputPath.size() - 5);
-        replace(outputPath.begin(), outputPath.end(), '\\', '/');
-        const char *start = strrchr(outputPath.c_str(), '/');
-        if (start)
-            outputPath.erase(0, start - outputPath.c_str() + 1);
-    }
-    
-    return outputPath;
-}
-
 bool OBSApp::do_mkdir(const char *path) {
     if (os_mkdirs(path) == MKDIR_ERROR) {
         blog(LOG_ERROR, "Failed to create directory %s", path);
         return false;
     }
-    
-    return true;
-}
-
-bool OBSApp::MakeUserProfileDirs() {
-    char path[512];
-    
-    if (GetConfigPath(path, sizeof(path), "obs-studio/basic/profiles") <= 0)
-        return false;
-    if (!do_mkdir(path))
-        return false;
-    
-    if (GetConfigPath(path, sizeof(path), "obs-studio/basic/scenes") <= 0)
-        return false;
-    if (!do_mkdir(path))
-        return false;
     
     return true;
 }
@@ -288,41 +137,6 @@ bool OBSApp::InitGlobalConfig() {
         return false;
     }
     
-    if (!opt_starting_collection.empty()) {
-        string path = GetSceneCollectionFileFromName(opt_starting_collection.c_str());
-        if (!path.empty()) {
-            config_set_string(globalConfig,
-                              "Basic", "SceneCollection",
-                              opt_starting_collection.c_str());
-            config_set_string(globalConfig,
-                              "Basic", "SceneCollectionFile",
-                              path.c_str());
-            changed = true;
-        }
-    }
-    
-    if (!opt_starting_profile.empty()) {
-        string path = GetProfileDirFromName(opt_starting_profile.c_str());
-        if (!path.empty()) {
-            config_set_string(globalConfig, "Basic", "Profile",
-                              opt_starting_profile.c_str());
-            config_set_string(globalConfig, "Basic", "ProfileDir",
-                              path.c_str());
-            changed = true;
-        }
-    }
-    
-    if (!config_has_user_value(globalConfig, "General", "Pre19Defaults")) {
-        uint32_t lastVersion = config_get_int(globalConfig, "General",
-                                              "LastVersion");
-        bool useOldDefaults = lastVersion &&
-        lastVersion < MAKE_SEMANTIC_VERSION(19, 0, 0);
-        
-        config_set_bool(globalConfig, "General", "Pre19Defaults",
-                        useOldDefaults);
-        changed = true;
-    }
-    
     // write config file to disk
     if (changed)
         config_save_safe(globalConfig, "tmp", nullptr);
@@ -334,11 +148,8 @@ bool OBSApp::InitGlobalConfig() {
 bool OBSApp::InitGlobalConfigDefaults() {
     config_set_default_string(globalConfig, "General", "Language",
                               DEFAULT_LANG);
-    config_set_default_uint(globalConfig, "General", "MaxLogs", 10);
     config_set_default_string(globalConfig, "General", "ProcessPriority",
                               "Normal");
-    config_set_default_bool(globalConfig, "General", "EnableAutoUpdates",
-                            true);
     
 #if _WIN32
     config_set_default_string(globalConfig, "Video", "Renderer",
@@ -347,24 +158,6 @@ bool OBSApp::InitGlobalConfigDefaults() {
     config_set_default_string(globalConfig, "Video", "Renderer", "OpenGL");
 #endif
     
-    config_set_default_bool(globalConfig, "BasicWindow", "PreviewEnabled",
-                            true);
-    config_set_default_bool(globalConfig, "BasicWindow",
-                            "PreviewProgramMode", false);
-    config_set_default_bool(globalConfig, "BasicWindow",
-                            "SceneDuplicationMode", true);
-    config_set_default_bool(globalConfig, "BasicWindow",
-                            "SwapScenesMode", true);
-    config_set_default_bool(globalConfig, "BasicWindow",
-                            "SnappingEnabled", true);
-    config_set_default_bool(globalConfig, "BasicWindow",
-                            "ScreenSnapping", true);
-    config_set_default_bool(globalConfig, "BasicWindow",
-                            "SourceSnapping", true);
-    config_set_default_bool(globalConfig, "BasicWindow",
-                            "CenterSnapping", false);
-    config_set_default_double(globalConfig, "BasicWindow",
-                              "SnapDistance", 10.0);
     config_set_default_bool(globalConfig, "BasicWindow",
                             "RecordWhenStreaming", false);
     config_set_default_bool(globalConfig, "BasicWindow",
--- obs-lite/Classes/ui/obs_app.hpp
+++ obs-lite/Classes/ui/obs_app.hpp
@@ -25,23 +25,19 @@ private:
     ConfigFile globalConfig;
     
     bool MakeUserDirs();
-    bool MakeUserProfileDirs();
     bool do_mkdir(const char *path);
-    std::string GetSceneCollectionFileFromName(const char *name);
-    std::string GetProfileDirFromName(const char *name);
     bool InitGlobalConfig();
     bool InitGlobalConfigDefaults();
     
 public:
     // get config file path
     static int GetConfigPath(char* path, size_t size, const char* name);
-    static void AddExtraModulePaths();
-    static int GetProgramDataPath(char *path, size_t size, const char *name);
-    static char* GetProgramDataPathPtr(const char *name);
     
+    // ctor & dtor
     OBSApp(profiler_name_store_t *store);
     virtual ~OBSApp();
     
+    // startup
     void RegisterStaticModuleLoader(OBS_STATIC_MODULE_LOADER loader);
     bool StartupOBS(const char* locale);
     
