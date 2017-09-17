78f21c9aff630315deb686a46ba835b53e525367
luma <stubma@gmail.com>
2017-09-17 21:13:19 +0800

    obs-mobile skeleton

---
+++ obs-mobile/proj.ios/Classes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  obs-mobile
+//
+//  Created by maruojie on 2017/9/17.
+//  Copyright © 2017年 shanshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ obs-mobile/proj.ios/Classes/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  obs-mobile
+//
+//  Created by maruojie on 2017/9/17.
+//  Copyright © 2017年 shanshan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
